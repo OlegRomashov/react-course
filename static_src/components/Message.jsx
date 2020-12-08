8ee8789c53000142c250d42a16d257674b1fdbe8
OlegRomashov <dead_line@list.ru>
2020-12-08 11:41:52 +0300

    added styles

--- static_src/components/Message.jsx
+++ static_src/components/Message.jsx
@@ -8,7 +8,9 @@ export default class Message extends React.Component {
   }
 
   render() {
-    return (<div>
+    return (<div
+      className="message"
+      style={{ alignSelf: this.props.name === 'bot' ? 'flex-start' : 'flex-end' }}>
       <strong> {this.props.name} : </strong>
       {this.props.content}
     </div>)
--- static_src/components/MessageField.jsx
+++ static_src/components/MessageField.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import Message from './Message'
+import './styles/styles.css'
 
 export default class MessageField extends React.Component {
   state = {
     messages: [
-      { name: 'robot', content: 'Привет !' },
-      { name: 'robot', content: 'Как дела ?' },
+      { name: 'bot', content: 'Привет !' },
+      { name: 'bot', content: 'Как дела ?' },
     ],
   }
   handleClick = () => {
-    this.setState({ messages: [...this.state.messages, { name: 'I', content: 'Нормально!' }] })
+    this.setState({ messages: [...this.state.messages, { name: 'me', content: 'Нормально!' }] })
   }
 
   componentDidUpdate() {
-    if (this.state.messages[this.state.messages.length - 1].name === 'I') {
+    if (this.state.messages[this.state.messages.length - 1].name === 'me') {
       const timeout = setTimeout(() =>
           this.setState(
             {
-              messages: [...this.state.messages, { name: 'robot', content: 'Не приставай ко мне, я робот!' }],
+              messages: [...this.state.messages, { name: 'bot', content: 'Не приставай ко мне, я робот!' }],
             }),
         1000)
     }
@@ -30,8 +31,10 @@ export default class MessageField extends React.Component {
   render() {
     const messageElements = this.state.messages.map((message, index) => (
       <Message key={index} name={message.name} content={message.content} />))
-    return <div>
-      {messageElements}
+    return <div className='layout'>
+      <div className='message-field'>
+        {messageElements}
+      </div>
       <button onClick={this.handleClick}> Отправить сообщение</button>
     </div>
   }
